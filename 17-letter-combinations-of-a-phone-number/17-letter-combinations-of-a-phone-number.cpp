811835f50ad9964adb56b4135687a623242f4466
ujjwal81 <81678908+ujjwal81@users.noreply.github.com>
2022-08-19 12:22:38 +0530

    Time: 4 ms (31.54%), Space: 6.7 MB (18.35%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    vector<string> back(string &digits,int i,map<int,string> &m){
+    vector<string> comb(string &digits,int i,map<int,string> &b){
         if(i==digits.size()){
             return {""};
         }
-        vector<string> temp=back(digits,i+1,m);
+        vector<string> temp=comb(digits,i+1,b);
         vector<string> ans;
-        for(auto a:m[digits[i]-'0']){
-            for(auto b:temp){
-                ans.push_back(a+b);
-            }
-        }
+        for(auto a:b[digits[i]-'0'])             //we are using backtracking
+            for(auto c:temp)
+                ans.push_back(a+c);
         return ans;
-        
     }
     vector<string> letterCombinations(string digits) {
-        map<int,string> m;
-        m[2]="abc";
-        m[3]="def";
-        m[4]="ghi";
-        m[5]="jkl";
-        m[6]="mno";
-        m[7]="pqrs";
-        m[8]="tuv";
-        m[9]="wxyz";
-        if(digits.size()==0){
+        if(digits.size()==0)
             return {};
-        }
-        return back(digits,0,m);
+        map<int,string> b;
+        b[2]="abc";
+        b[3]="def";
+        b[4]="ghi";
+        b[5]="jkl";
+        b[6]="mno";
+        b[7]="pqrs";
+        b[8]="tuv";
+        b[9]="wxyz";
+        return comb(digits,0,b);
     }
 };
